f31c1f823e7f99df2ccca969240e7ab5bf1e650c
Aaron <amanda.steph6@gmail.com>
2025-06-19 11:15:26 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+Scroll().reveal('.hero-content',{delay: 200, origin: 'left', distance: '50px'});
+Scroll().reveal('.hero-image',{delay:400, origin:'right', distance: '50px'});
+Scroll().reveal('section h2' ,{delay:200, origin: 'bottom', distance: '40px'});
+ScrollReveal().reveal('.card, .testimonial, form', { interval: 200 });
